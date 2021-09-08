44b16eaf6fdb53ed2f70c9fc1af7c6d6d6435ebb
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2021-09-08 10:06:26 +0200

    Rettet regneformular

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,11 +10,10 @@ public class Main {
 public static void main(String[] args) {
 // Exercise 2 tester hvad der sker når der bliver givet to format typer til en value
     Scanner in = new Scanner(System.in);
-    final double ONE_FAH_IN_CEL = 17.2222222;
 
     System.out.print("Hvor mange grader varmt er her? ");
     double gra = in.nextInt();
-    System.out.printf("%.1f grader er %.1f Fahrenheit\n", gra, ONE_FAH_IN_CEL * gra);
+    System.out.printf("%.1f grader er %.1f Fahrenheit\n", gra, gra*9/5+32);
 
 //Exercise 3 converts to total number of seconds
     System.out.print("tast antal sekunder: ");
